e121e44eb4fa5db69e92477159fd8b281f744ffe
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-07 09:25:10 -0800

    Addition of type definitions for testing

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,2 +1,6 @@
 
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="assertion-error/assertion-error.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="sinon/sinon.d.ts" />
