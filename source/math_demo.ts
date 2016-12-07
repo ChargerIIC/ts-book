0ea4ffec7f3752cefc86b009248a5f7c16cc5725
ChargerIIC <ChargerIIC@Yahoo.com>
2016-12-07 15:25:30 -0800

    Fix for typo in method name

--- source/math_demo.ts
+++ source/math_demo.ts
@@ -15,7 +15,7 @@ class MathDemo implements MathInterface{
     return result;
   }
 
-  public powAsyncSlow(base: number, exponent: number, cb : (result : number) => void) {
+  public powAsync(base: number, exponent: number, cb : (result : number) => void) {
     var delay = 45; //ms
     setTimeout(() => {
       var result = this.pow(base, exponent);
